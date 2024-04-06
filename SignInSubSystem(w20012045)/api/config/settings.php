2d87d52179ec9a5f2fb30bb725a3e32530204750
Shawryfall <121821378+Shawryfall@users.noreply.github.com>
2024-04-06 02:11:12 +0100

    Delete SignInSubSystem(w20012045) directory

--- SignInSubSystem(w20012045)/api/config/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 
- * @author Patrick Shaw
- */
-define('BASEPATH', '/kv6002/signinsubsystem/api');
-define('USERS_DATABASE', 'db/users.sqlite');
-define('SECRET', '7rTNO,(bR$MP,~k.+~MI2pcTBLYD[lhs%}l)1$33v8%0^p25NDCb*IKPubW:Y#+');
