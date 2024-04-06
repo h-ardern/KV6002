dae9ea811c0b754e13dc04bc5c65f762ed1a39d7
Shawryfall <121821378+Shawryfall@users.noreply.github.com>
2024-04-06 18:29:30 +0100

    Update Account.jsx

--- Dynamic-Search(w21034262)/src/pages/Account.jsx
+++ Dynamic-Search(w21034262)/src/pages/Account.jsx
@@ -1,34 +1,19 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../contexts/AuthContext';
-
 /**
  * 
  * @author Patrick Shaw
-*/
+ */
 function Account() {
-  const { state, signOut } = useAuthContext();
-  const { signedIn, userType, gender, userId, interest } = state;
+  const { state } = useAuthContext();
+  const { signedIn, userType, gender, userId, interests, firstName, lastName, age, username, email, address } = state;
   const navigate = useNavigate();
-  const [details, setDetails] = useState([]);
 
   useEffect(() => {
     if (!signedIn) {
       navigate('/');
-      return;
     }
-
-    fetch("https://w20012045.nuwebspace.co.uk/kv6002/signinsubsystem/api/userdata", {
-      method: 'GET',
-      headers: new Headers({ "Authorization": "Bearer " + localStorage.getItem('token') }),
-    })
-      .then(response => {
-        return response.status === 200 ? response.json() : [];
-      })
-      .then(json => setDetails(json))
-      .catch(err => {
-        console.log(err.message);
-      });
   }, [signedIn, navigate]);
 
   if (!signedIn) {
@@ -39,21 +24,18 @@ function Account() {
     <>
       <div className="text-center my-4">
         <h2 className="text-2xl font-bold my-2">Account ({userType})</h2>
-        {userType === "participant"  && (<h2 className="text-2xl font-bold my-2">Interest: {interest}</h2>)}
+        {userType === "participant" && (
+          <h2 className="text-2xl font-bold my-2">Interests: {interests}</h2>
+        )}
         <h3 className="text-2xl font-bold my-2">ID: {userId}</h3>
         <div className="mt-4">
-          {details.map((detail, index) => (
-            <div key={index} className="mb-4">
-              <p>First Name: {detail.firstname}</p>
-              <p>Last Name: {detail.lastname}</p>
-              <p>Gender: {gender}</p>
-              <p>Age: {detail.age}</p>
-              <p>Username: {detail.username}</p>
-              <p>Email: {detail.email}</p>
-              <p>Address: {detail.address}</p>
-              
-            </div>
-          ))}
+          <p>First Name: {firstName}</p>
+          <p>Last Name: {lastName}</p>
+          <p>Gender: {gender}</p>
+          <p>Age: {age}</p>
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
+          <p>Address: {address}</p>
         </div>
       </div>
     </>
